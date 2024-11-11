1d0f5998d246eeb3d2db655f80a86fc0973982a8
Myat Pwint Phyu <mpwintphyu555@gmail.com>
2024-11-11 20:41:55 +0630

    index commit

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,22 @@
-alert("Hello");
+function updateTime() {
+  //los angeles
+
+  let lAElement = document.querySelector("#la");
+  let LAdateElement = lAElement.querySelector(".date");
+  let LATimeElement = lAElement.querySelector(".time");
+  let LATime = moment.tz("America/Los_Angeles");
+  LAdateElement.innerHTML = LATime.format("MMMM Do YYYY");
+  LATimeElement.innerHTML = LATime.format("h:mm:ss [<small>]A[</small>]");
+
+  //Sydney
+  let SydneyElement = document.querySelector("#sydney");
+  let SydneyDateElement = SydneyElement.querySelector(".date");
+  let SydneyTimeElement = SydneyElement.querySelector(".time");
+  let SydneyTime = moment.tz("Australia/Sydney");
+  SydneyDateElement.innerHTML = SydneyTime.format("MMMM Do YYYY");
+  SydneyTimeElement.innerHTML = SydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
